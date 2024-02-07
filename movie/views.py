be7ee7181a6222d3bfb20fec30ec0ecf260a3c28
darango378 <darango378@gmail.com>
2024-02-07 13:56:43 -0500

    Busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Movie
 # Create your views here.
 
 def home (request):
     #return HttpResponse('<H1>Welcome to homepage</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Daniela Arango'})
+    #return render(request, 'home.html', {'name':'Daniela Arango'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
 
 def about(request):
      #return HttpResponse("This is the About page")
