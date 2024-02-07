c92926e69af60436a8c2413c5add3bbdfb980a4b
darango378 <darango378@gmail.com>
2024-02-07 10:59:37 -0500

    primer commit

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home (request):
+    #return HttpResponse('<H1>Welcome to homepage</h1>')
+    #return render(request, 'home.html')
+    return render(request, 'home.html', {'name':'Greg Lim'})
