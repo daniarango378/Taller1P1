888d798b1d2acc4bae5cfde7069568a8ee8765ad
darango378 <darango378@gmail.com>
2024-02-07 13:08:29 -0500

    all project

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description= models.CharField(max_length=250)
+    image = models.ImageField(upload_to='movie/images', height_field=None, width_field=None, max_length=None)
+    url= models.URLField(blank=True)
